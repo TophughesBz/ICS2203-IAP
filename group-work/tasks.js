2c100ab8519f72c85d7f0cfa8928a7737de3f03e
Gift Nestah <94760907+PantheraNestah@users.noreply.github.com>
2023-11-24 14:18:04 +0300

    Update tasks.js and added group members registration numbers

--- group-work/tasks.js
+++ group-work/tasks.js
@@ -1,3 +1,12 @@
+/**
+  * GROUP MEMBERS:-
+  * SCT211-0069/2022 Methusella Misiko
+  * SCT211-0071/2022 Gift Nestah
+  * SCT211-0006/2022 Chrispin Maina
+  * SCT211-0765/2022 Meshack Otieno
+**/
+
+
 var submit = document.querySelector("input#submit")
 var newTask = document.querySelector("input#tasks")
 
